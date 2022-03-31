92b57c2cca9157adf0a678e2f7f3351effb82cde
chanomon <hdomeli@gmail.com>
2022-03-31 11:51:06 -0600

    Update drapmodulesv0-1.py

--- drapmodulesv0-1.py
+++ drapmodulesv0-1.py
@@ -3,7 +3,7 @@
 #
 # NOAAa DRAP model implementation
 # 
-# Copyright (C) 2021  Elizandro Huipe Domratcheva (hdomeli@gmail.com)
+# Copyright (C) 2021 Author:Elizandro Huipe Domratcheva (hdomeli@gmail.com)
 #                      
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
